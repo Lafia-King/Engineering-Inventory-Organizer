9067e6a54856679936fad2d352729683be262261
ChloeAcheampong <chloeacheampong@gmail.com>
2015-11-21 08:21:06 +0000

    created a skeleton of the maufacturer class

---
+++ manufacturer.php
@@ -0,0 +1,26 @@
+<?php
+	include_once("dbconnection.php");
+	
+	class manufactuerer extends dbconnection{
+		function manufactuerer(){
+			dbconnection::dbconnection();
+		}
+		
+		function get_all_manufactuerer(){
+		}
+		
+		function add_manufactuerer(){
+		}
+		
+
+		function update_manufactuerer(){
+		}
+
+		
+		function delete_manufactuerer(){
+		}
+	}
+	
+	
+	
+?>

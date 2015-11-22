1bde605e49954935abb1a6eb2e6ccadc31026fdf
ChloeAcheampong <chloeacheampong@gmail.com>
2015-11-22 13:19:01 +0000

    created a structure for the manufacturer class

---
+++ manufacturer.php
@@ -0,0 +1,24 @@
+<?php
+	include_once("dbconnection.php");
+	
+	class manufacturer extends dbconnection{
+		function manufacturer(){
+			dbconnection::dbconnection();
+		}
+		
+		function get_all_manufacturer(){
+		}
+		
+		function add_manufacturer(){
+			
+		}
+		
+
+		function update_manufacturer()
+		}
+
+		
+	}
+
+	
+?>

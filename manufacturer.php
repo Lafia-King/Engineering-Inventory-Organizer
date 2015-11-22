3d656c2bcb365e66761d49d800619059fd3cebeb
ChloeAcheampong <chloeacheampong@gmail.com>
2015-11-22 22:19:37 +0000

    created a function to add a manufacturer to the database. And corrected the class to use the phpDocumentor code format

--- manufacturer.php
+++ manufacturer.php
@@ -1,17 +1,46 @@
 <?php
 	include_once("dbconnection.php");
 	
-	class manufacturer extends dbconnection{
-		function manufacturer(){
+	  /**
+	*This is a class implemented to interact with the manufacturer
+	*database.
+	*/
+	class Manufacturer extends dbconnection{
+
+		/**
+		*A constructor to create the database connection
+		*/
+		function Manufacturer(){
 			dbconnection::dbconnection();
 		}
 		
-		function get_all_manufacturers(){
+
+
+		/**
+		*query to add a record to family planning records 
+		*@return  all manufacturers in the database
+		*/
+		function getAllManufacturers(){
 			$query="select * from manufacturer";
 			return $this->query($query);
 		}
-		
-		function add_manufacturer(){
+
+
+		/**
+		*query to add a manufacturer to manufacturer database
+		* 
+		*@param string $name
+		*@param string $email
+		*@param string $phone_number
+		*@param string $website
+		*@param string $country
+		*@return  true or false - successful or unsuccessful
+		*/
+		function addManufacturer($name, $email, $phone_number, $website, $country){
+			$query ="insert into manufacturer set name='$name',
+			email=$email, phone_number=$phone_number, website=$website, 
+			country=$country";
+			return $this->query($query);
 			
 		}
 		

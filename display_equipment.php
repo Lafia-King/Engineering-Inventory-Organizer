7481f2443c1bbedff2a8dbd1a1cc6454f3ef1fef
yaa-amoako <phoebe.priscy@gmail.com>
2015-11-23 21:56:28 +0000

    Added to the implementation for viewing all equipment

--- display_equipment.php
+++ display_equipment.php
@@ -11,6 +11,42 @@
 		$newView->getEquipment();		
 		$row = $newView->fetch();
 		
+				echo ("<table class='table table-condensed table-hover table-bordered'>");
+			echo("<tr><th>Name</th>
+				  <th>Equipment ID</th>
+				  <th>Category ID</th>
+				  <th>Storage Location</th>
+				  <th>Current Condition</th>
+				  <th>Label</th>
+				  <th>Borrow Status</th> </tr> ");
+					
+			while ($row) {
+				$x = $row['id'];
+				echo ("<tr><td>");
+				echo ( "<a href = 'display_single_equipment.php?id=$x'>".  $row['name']." </a>");
+				echo ("</td>");
+				echo ("<td>");
+				echo $row["equipment_id"];
+				echo ("</td>");
+				echo ("<td>");
+				echo $row["category_id"];
+				echo ("</td>");
+				echo ("<td>");
+				echo $row["storage_location"];
+				echo ("</td>");
+				echo ("<td>");
+				echo $row["current_condition"];
+				echo ("</td>");
+				echo ("<td>");
+				echo $row["label"];
+				echo ("</td>");
+				echo ("<td>");
+				echo $row["borrow_status"];
+				echo ("</td></tr>");
+				$row = $newView->fetch();
+			}
+			echo ("</table>");
+		
 ?>
 </body> 
 </html>

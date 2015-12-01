bf1ef88fd0f26fa7a58eff117300a2c507fd8a92
ChloeAcheampong <chloeacheampong@gmail.com>
2015-12-01 18:29:55 +0000

    created a function to add manufacturer

--- manufacturer.php
+++ manufacturer.php
@@ -1,26 +1,55 @@
 <?php
 	include_once("dbconnection.php");
 	
-	class manufactuerer extends dbconnection{
-		function manufactuerer(){
+	  /**
+	*This is a class implemented to interact with the manufacturer
+	*database.
+	*/
+	class Manufacturer extends dbconnection{
+
+		/**
+		*A constructor to create the database connection
+		*/
+		function Manufacturer(){
 			dbconnection::dbconnection();
 		}
 		
-		function get_all_manufactuerer(){
+
+
+		/**
+		*query to get manufactures
+		*@return  all manufacturers in the database
+		*/
+		function getAllManufacturers(){
+			$query="select * from manufacturer";
+			return $this->query($query);
 		}
-		
-		function add_manufactuerer(){
+
+
+		/**
+		*query to add a manufacturer to manufacturer database
+		* 
+		*@param string $name
+		*@param string $email
+		*@param string $phone_number
+		*@param string $website
+		*@param string $country
+		*@return  true or false - successful or unsuccessful
+		*/
+		function addManufacturer($name, $email, $phone_number, $website, $country){
+			$query ="insert into manufacturer set name='$name',
+			email=$email, phone_number=$phone_number, website=$website, 
+			country=$country";
+			return $this->query($query);
+			
 		}
 		
 
-		function update_manufactuerer(){
+		function update_manufacturer()
 		}
 
 		
-		function delete_manufactuerer(){
-		}
 	}
-	
-	
+
 	
 ?>

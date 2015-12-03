f3a13ad1882c434f58c6b845b2c6fd5b86783553
yaa-amoako <phoebe.priscy@gmail.com>
2015-12-03 20:59:54 +0000

    Updated model with tentative queries for db--V3

--- Dashboard/viewEquipment.php
+++ Dashboard/viewEquipment.php
@@ -13,6 +13,10 @@
 	/**
 	 *@method array getEquipment()
 	 *@method array getSingleEquipment($id)
+	 *@method array getBorrowedEquipment()
+	 *@method array getOverdueEquipment()
+	 *@method array getDueEquipment()
+	 *@method array getBrokenEquipment()
 	 */
 		
 		/**
@@ -26,6 +30,34 @@
 		 */
 		function getSingleEquipment($id){
 			return $this->query("select * from equipment where id=$id");
+		}
+		
+		/**
+		 *@return list Returns all borrowed equipment from database
+		 */
+		function getBorrowedEquipment(){
+			return $this->query("select * from equipment where borrow_status = 'Borrowed'");
+		}
+		
+		/**
+		 *@return list Returns all overdue equipment from database
+		 */
+		function getOverdueEquipment(){
+			return $this->query("select * from equipment where dateReturn < cast((now()) as date)");
+		}	
+		
+		/**
+		 *@return list Returns all equipment due today from database
+		 */
+		function getDueEquipment(){
+			return $this->query("select * from equipment where dateReturn=cast(now() as date");
+		}
+		
+		/**
+		 *@return list Returns all broken equipment from database
+		 */
+		function getBrokenEquipment(){
+			return $this->query("select * from equipment where state='bad'");
 		}
 	}
 	?>

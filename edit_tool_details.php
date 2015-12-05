abf271027e5c86442e31f79159ae6262f7f880b6
Sugruali <sugruali@gmail.com>
2015-12-05 01:32:47 +0000

    updates

---
+++ edit_tool_details.php
@@ -0,0 +1,24 @@
+	function add_tool(){
+	
+		$tool_id=$_REQUEST['tool_id'];
+		$barcode=$_REQUEST['barcode'];
+		$name=$_REQUEST['tool_name'];
+		$manufacturer=$_REQUEST['manufacturer-name'];
+		$supplier=$_REQUEST['supplier_name'];
+		$price=$_REQUEST['price'];
+		$date_purchased= date('Y-m-d', strtotime($_REQUEST['date-bought']));
+		$repair_date= date('Y-m-d', strtotime($_REQUEST['repair-date']));
+		$condition=$_REQUEST['condition'];
+		$location=$_REQUEST['location'];
+		$department=$_REQUEST['department'];
+
+
+		$querry="UPDATE `items` 
+		SET`tool_id`, `barcode`, `item_name`, `manufacturer_name `supplier_name`, `price`, `date_bought`, `last_repair_date`,
+			 `condition`, `location`, `department`
+			WHERE tool_id=$tool_id
+		
+
+		return $this->query($querry);
+
+	}

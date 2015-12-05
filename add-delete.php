c8ba573abf21532698790ec03627b524dc8c2e2a
Sugruali <sugruali@gmail.com>
2015-12-05 03:31:41 +0000

    changed the parameters in this function to match the database

--- add-delete.php
+++ add-delete.php
@@ -1,34 +1,37 @@
 	function add_tool(){
 		if (isset($_REQUEST['item-code'])) {
-		$tool_id=$_REQUEST['tool_id'];
+		$equipment_id=$_REQUEST['equipment_id'];
+		$product_id=$_REQUEST['product_id'];
+		$category_id=$_REQUEST['category_id'];
+		$storage_location=$_REQUEST['storage_location'];
 		$barcode=$_REQUEST['barcode'];
-		$name=$_REQUEST['tool_name'];
-		$manufacturer=$_REQUEST['manufacturer-name'];
-		$supplier=$_REQUEST['supplier_name'];
+		$name=$_REQUEST['name'];
+		$manufacturer_id=$_REQUEST['manufacturer_id'];
+		$supplier_id=$_REQUEST['supplier_id'];
 		$price=$_REQUEST['price'];
-		$date_purchased= date('Y-m-d', strtotime($_REQUEST['date-bought']));
+		$date_created= date('Y-m-d', strtotime($_REQUEST['date_created']));
 		$repair_date= date('Y-m-d', strtotime($_REQUEST['repair-date']));
-		$condition=$_REQUEST['condition'];
+		$current_condition=$_REQUEST['current_condition'];
+		$label=$_REQUEST['label'];
+		$asset_type=$_REQUEST['asset_type'];
+		$borrow_status=$_REQUEST['borrow_status'];
 		$location=$_REQUEST['location'];
 		$department=$_REQUEST['department'];
 
 
-		$querry="INSERT INTO `items` (`item_number`, `barcode_number`, 
-			`item_name`, `manufacturer`, `supplier_name`, `price`, `date_bought`, `last_repair_date`,
-			 `conditions`, `location`, `department`) 
-			VALUES ('$item_no', '$barcode', '$name', '$manufacturer', `supplier_name`, '$price', '$date_bought', 
-			'$repair_date', '$condition', '$location', '$department');";
+		$querry="INSERT INTO `items` (`equipment_id`, 'product_id', 'category_id', 'storage_location', `barcode`,'name',`manufacturer_id`, `supplier_id`, `price`, `date_created`, `repair_date`,
+			 `current_conditions`, 'asset_type','borrow_status',`location`, `department`) 
+			VALUES ('$equipment_id`', '$product_id', '$category_id','$storage_location','$barcode','$manufacturer_id', `supplier_id`, '$price', '$date_created','$repair_date' 
+			 '$current_condition','asset_type','$borrow_status', '$location', '$department');";
 		
 
 		return $this->query($querry);
 	}
 
-	}
-	
-	
+		
 	
 		function delete_item(){
-		$no=$_REQUEST['id'];
+		$no=$_REQUEST['equipment_id'];
 		$querry="DELETE FROM items WHERE item_number=$no";
 
 		return $this->query($querry);
